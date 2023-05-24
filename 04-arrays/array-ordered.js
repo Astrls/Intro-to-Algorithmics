fdf5187b8baa021bfcbe44103f6aeb08a439224a
Antoine Struelens <antoine.struelens@gmail.com>
2023-05-24 11:01:49 +0200

    correction for arrays

--- 04-arrays/array-ordered.js
+++ 04-arrays/array-ordered.js
@@ -1,21 +1,26 @@
-// allow input from terminal
+// Allows input from terminal
 const prompt = require("prompt-sync")();
 
+//Determine length of the array
+const LEN = parseInt((prompt("Choose the length of the array ")));
+
+// Arrays
 tab = [];
 verif = [];
 
-//user input
-for (let i = 0; i < 4 ; i++) {
+// User enters numbers
+for (let i = 0; i < LEN ; i++) {
     tab.push(parseInt((prompt("Type in any number: "))));
 }
 
+// Displays the array of numbers
 console.log(tab);
 
-const N = (tab.length -1); // = 4
+// Compares each element to the next and verify if true or false
+const N = (tab.length -1);
 
-
-for (let i = 1 ; i <= N ; i++){
-    if (tab[N] >= tab[N-i] >= tab[N-i-1] ) {
+for (let i = 0 ; i < N ; i++){
+    if (tab[i] <= tab[i+1]) { 
         verif.push(true);
     } 
     else {
@@ -23,16 +28,15 @@ for (let i = 1 ; i <= N ; i++){
     }
 } 
 
+// Displays the results of the comparison in an array
 console.log(verif);
 
+
+// Prints the final result: is the array in ascending order?
 if (verif[0,1,2] == true) {
     console.log("TRUE")
 }
 else {
     console.log("FALSE")
 }
-
-
-
-
 
